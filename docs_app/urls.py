922c0ea79afee1cdd325260d7537d854b0ffc03d
unknown <mmohamedabdelm@gmail.com>
2023-08-18 21:58:33 +0300

    add CRUD operation for Category and Documents

--- docs_app/urls.py
+++ docs_app/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import List_Category, Document_List, Document_Detail
+from .views import (Document_List,List_Category, Document_Detail,
+                    Create_Category, Create_Document, Update_Category,
+                    Update_Document,Delete_Category, Delete_Document)
+                    
 urlpatterns = [
     path('', List_Category.as_view(), name='list_category'),
     path('category/<int:id>', Document_List, name='document_list'),
     path('category/<int:id>/document/<int:pk>', Document_Detail.as_view(), name='document_detail'),
+    path('create_category', Create_Category.as_view(), name='create_category'),
+    path('create_document', Create_Document.as_view(), name='create_document'),
+    path('update_category/<int:pk>', Update_Category.as_view(), name='update_category'),
+    path('update_document/<int:pk>', Update_Document.as_view(), name='update_document'),
+    path('delete_category/<int:pk>', Delete_Category.as_view(), name='delete_category'),
+    path('delete_document/<int:pk>', Delete_Document.as_view(), name='delete_document'),
 ]
--- docs_app/views.py
+++ docs_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Category, Document
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # Create your views here.
 
 class List_Category(ListView):
@@ -21,3 +21,46 @@ class Document_Detail(DetailView):
     template_name = 'doc/document_detail.html'
    
 
+
+
+
+
+class Create_Category(CreateView):
+    model = Category
+    template_name = 'doc/create_category.html'
+    fields = '__all__'
+    success_url = '/'
+
+
+class Create_Document(CreateView):
+    model = Document
+    template_name = 'doc/create_document.html'
+    fields = '__all__'
+    success_url = '/'    
+
+
+class Update_Category(UpdateView):
+    model = Category
+    fields = '__all__'    
+    template_name = 'doc/update_form.html'
+    success_url = '/'    
+
+
+class Update_Document(UpdateView):
+    model = Document
+    fields = '__all__'    
+    template_name = 'doc/update_form.html'
+    success_url = '/'    
+
+
+class Delete_Category(DeleteView):
+    model = Category
+    success_url = '/'    
+    template_name = 'delete_category.html'
+
+
+
+class Delete_Document(DeleteView):
+    model = Document
+    success_url = '/'    
+    template_name = 'delete_document.html'
